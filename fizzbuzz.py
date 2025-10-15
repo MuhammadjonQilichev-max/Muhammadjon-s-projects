61dfe708b849a54b660ab6effc6780c7597bce95
MuhammadjonQilichev-max <muhammadqilichev081@gmail.com>
2025-10-15 10:40:08 +0500

    Add fizzbuzz.py, A simple mad style program

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+print("--- The 'FizzBuzz' Challenge ---")
+for i in range(1, 101):
+    if (i%15) == 0:
+        print('BuzzFizz')
+    elif (i%3) == 0:
+        print('Buzz')
+    elif (i%5) == 0 :
+        print("Fizz")
+    else:
+        print(i)
+
